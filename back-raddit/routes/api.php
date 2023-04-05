cf570ece6fe60abe970ad901d8dc703390b932fe
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-05 16:41:58 +0200

    route home

--- back-raddit/routes/api.php
+++ back-raddit/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('', [HomeController::class, 'display']);
+Route::get('/', [PostsController::class, 'display']);
