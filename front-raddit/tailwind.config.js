d216bcb257c6dde5e276aae275c78edd3d53699c
niiko <nicolas.rocagel@epitech.eu>
2023-04-05 11:41:43 +0200

    set-up

---
+++ front-raddit/tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{html,js,vue,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'btn-color': '#243c5a'
+      }
+    }
+  },
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter'
+    ]
+  },
+  fontFamily: {
+    Roboto: ['Roboto, sans-serif']
+  },
+  container: {
+    padding: '2rem',
+    center: true
+  },
+  screens: {
+    sm: '640px',
+    md: '768px'
+  },
+  plugins: [require('daisyui')]
+}
