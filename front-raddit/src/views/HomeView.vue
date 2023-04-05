2e95e1f9978a3155b0749c02ae77d880cc4981fe
dinoBALLETTI <db00291@hotmail.com>
2023-04-05 18:30:07 +0200

    setting up Tailwind + DaisyUI

--- front-raddit/src/views/HomeView.vue
+++ front-raddit/src/views/HomeView.vue
@@ -5,11 +5,10 @@ import NavBar from '../components/NavBarComponent.vue'
 
 <template>
   <head>
-
-
     <NavBar></NavBar>
   </head>
+
   <body>
-    <div>Hello</div>
+    <div><button class="btn btn-primary">Button</button></div>
   </body>
 </template>
