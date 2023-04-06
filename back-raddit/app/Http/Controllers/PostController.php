cafe7dd7ee9020b0f84ed813a48b10db84584801
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-06 17:23:43 +0200

    save work

--- back-raddit/app/Http/Controllers/PostController.php
+++ back-raddit/app/Http/Controllers/PostController.php
@@ -7,14 +7,29 @@ use App\Models\Posts;
 
 class PostController extends Controller
 {    
-    public function index()
+    public function display()
     {
         $posts = Posts::all();
         return response()->json($posts);
     }
+    
+    public function displayOne(string $title) {
+        $posts = Posts::all()->where('title', $title);
+        return response()->json($posts);
+    }
+    
+    public function storePost(Request $request) {
+        $post = new Posts;
 
-    public function create() {
-        
+        $post->author_id = "2";
+        $post->subraddit_name = "Rosenbaum LLC";
+        $post->title = "Test article 2";
+        $post->content = "Test contenu 2";
+
+        $post->save();
+        dd($post);
+
+        return Posts::all();
     }
 
     public function edit() {
--- back-raddit/routes/api.php
+++ back-raddit/routes/api.php
@@ -22,7 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Routing Posts
-Route::get('/post', [PostController::class, 'index']);
+Route::get('/post', [PostController::class, 'display']);
+Route::get('/post/{title}', [PostController::class, 'displayOne']);
+Route::post('/post/store', [PostController::class, 'storePost']);
+Route::put('/post/{title}', [PostController::class, 'editPost']);
+Route::delete('/post/{title}', [PostController::class, 'deletePost']);
 
 
 Route::get('/subraddit', [SubRadditController::class, 'display']);
