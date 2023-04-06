d3361bbf353911c9c70d6492f2fcecae14025d27
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-06 10:37:49 +0200

    save work

--- back-raddit/routes/api.php
+++ back-raddit/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubRadditController;
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', [PostsController::class, 'display']);
+// Routing Posts
+Route::get('/', [HomeController::class, 'show']);
+Route::resource('posts', PostsController::class);
 
 Route::get('/subraddit', [SubRadditController::class, 'display']);
