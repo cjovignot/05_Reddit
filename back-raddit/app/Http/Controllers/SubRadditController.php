17920539893707cdc6bdef5451bb708e4c9f457b
niiko <nicolas.rocagel@epitech.eu>
2023-04-06 12:23:14 +0200

    finish get for all gitraddit and filtered with an id

--- back-raddit/app/Http/Controllers/SubRadditController.php
+++ back-raddit/app/Http/Controllers/SubRadditController.php
@@ -2,13 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use App\Models\Subraddits;
 
+
+/**
+ * @group SubRaddit Management
+ * 
+ * APIs to manage the subraddit resources.
+ */
 class SubRadditController extends Controller
 {
-    //
-    public function display()
+    /**
+     * Display a listing of the resource.
+     */
+    public function displayAll()
     {
-        return "coucou subraddit";
+
+        // $subraddit = Subraddits::find($subrId)
+        $subraddit = Subraddits::all();
+        return $subraddit;
+    }
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display a subraddit info.
+     * 
+     * You need to add the subraddit id in the param
+     */
+    public function displayOne(string $id)
+    {
+        $subradditAll = Subraddits::all();
+        $subraddit = $subradditAll->find($id);
+        // dd($subraddit);
+        return $subraddit->toJson(JSON_PRETTY_PRINT);;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
