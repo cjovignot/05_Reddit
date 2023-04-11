950511b589ad6897e7a2975531653c31db1080e4
niiko <nicolas.rocagel@epitech.eu>
2023-04-11 17:41:58 +0200

    finish log out

--- back-raddit/config/cors.php
+++ back-raddit/config/cors.php
@@ -15,19 +15,19 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/(.*)\.wip/', '/(.*)\. test/'],
 
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 32839238,
 
     'supports_credentials' => true,
 
