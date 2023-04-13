d93395720afe3563459ae99b0168358b4f44b179
dinoBALLETTI <db00291@hotmail.com>
2023-04-14 00:55:54 +0200

    changes on top fields display

--- front-raddit/src/views/HomeView.vue
+++ front-raddit/src/views/HomeView.vue
@@ -26,10 +26,10 @@ window.onscroll = () => {
    <LeftPanelComponent class="hidden" />
     <section class="page">
     <div class="ctnr_top">
-      <div class="ctnr_field topSubRaddit1"><img src="../assets/images/RadditsField.png" /></div>
-      <div class="ctnr_field topSubRaddit2"><img src="../assets/images/RadditsField.png" /></div>
-      <div class="ctnr_field topSubRaddit3"><img src="../assets/images/RadditsField.png" /></div>
-      <div class="ctnr_field topSubRaddit4"><img src="../assets/images/RadditsField.png" /></div>
+      <div class="ctnr_field topSubRaddit1"><img src="../assets/images/TopFields.png" />SubRaddit 1</div>
+      <div class="ctnr_field topSubRaddit2"><img src="../assets/images/TopFields.png" />SubRaddit 2</div>
+      <div class="ctnr_field topSubRaddit3"><img src="../assets/images/TopFields.png" />SubRaddit 3</div>
+      <div class="ctnr_field topSubRaddit4"><img src="../assets/images/TopFields.png" />SubRaddit 4</div>
     </div>
 
       <!-- *** FILTERBAR *** -->
