57bb55946c9aac8c2000e66edba24fcffe0851a4
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-04-14 14:45:00 +0200

    saving user page

--- front-raddit/src/components/TopVignettesComponent.vue
+++ front-raddit/src/components/TopVignettesComponent.vue
@@ -1,26 +1,41 @@
 <script setup>
 import { ref } from 'vue'
 
+import * as dayjs from 'dayjs';
+import * as relativeTime from 'dayjs/plugin/relativeTime';
+import * as calendar from 'dayjs/plugin/calendar';
+
+dayjs.extend(relativeTime);
+dayjs.extend(calendar)
+
 let user_infos = ref({});
 let banner_url = ref('');
+let name = ref('');
+let about = ref('');
 user_infos.value = localStorage.user
 
-banner_url.value = JSON.parse(user_infos.value);
-console.log(banner_url.value);
+user_infos.value = JSON.parse(user_infos.value);
+console.log(user_infos.value.name);
+banner_url.value = user_infos.value.banner_picture_URL;
+name.value = user_infos.value.name;
+about.value = user_infos.value.created_at;
+
 </script>
 
 
 <template>
-    <!-- {{ url }} -->
 <div class="card w-96 bg-base-100 shadow-xl">
-  <figure><img :src=banner_url.value alt="" /></figure>
+  <figure><img :src=banner_url alt="" /></figure>
   <div class="card-body">
-    <h2 class="card-title">UserName</h2>
-    <p>About</p>
+    <h2 class="card-title">{{name}}</h2>
+    <p>Registered since : {{ dayjs(about).calendar() }}</p>
   </div>
 </div>
 </template>
 
 <style scoped>
-
+img {
+    width: 100%;
+    height: auto;
+}
 </style>
