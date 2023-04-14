0dd08c4f9f5e2c9767fa55472dc733057dec19fd
niiko <nicolas.rocagel@epitech.eu>
2023-04-14 14:58:47 +0200

    change header display

--- front-raddit/src/views/SubradditView.vue
+++ front-raddit/src/views/SubradditView.vue
@@ -16,72 +16,46 @@ const subradditStore = useSubradditStore()
 subradditStore.getSubInfo(url.params.subraddit_name)
 
 window.onscroll = () => {
-        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
-        if (bottomOfWindow) {
-          subradditStore.page++ 
-          console.log(subradditStore.page)
-          subradditStore.getSubPosts();
-        }
-      }
-
+  let bottomOfWindow =
+    document.documentElement.scrollTop + window.innerHeight ===
+    document.documentElement.offsetHeight
+  if (bottomOfWindow) {
+    subradditStore.page++
+    console.log(subradditStore.page)
+    subradditStore.getSubPosts()
+  }
+}
 </script>
 
 <template>
-
-<body>
-  <LeftPanelComponent class="hidden" />
-    <section class="page">
-    <div class="ctnr_top">
-      <div class="ctnr_field topSubRaddit1"><img src="../assets/images/RadditsField.png" /></div>
-      <div class="ctnr_field topSubRaddit2"><img src="../assets/images/RadditsField.png" /></div>
-      <div class="ctnr_field topSubRaddit3"><img src="../assets/images/RadditsField.png" /></div>
-      <div class="ctnr_field topSubRaddit4"><img src="../assets/images/RadditsField.png" /></div>
+  <div class="ctnr_top">
+    <div class="ctnr_field topSubRaddit1">
+      <h1>top posts</h1>
+      <img src="../assets/images/300pxBurriedRaddit.png" />Oui oui les baguettes 🥖
     </div>
-
-      <!-- *** FILTERBAR *** -->
-      <div class="flex flex-row bg-red-500 w-[78%] m-auto mb-10">
-        <div class="ctnr_new">🆕</div>
-        <div class="ctnr_hot">🔥</div>
-      </div>
-
+    <div class="ctnr_field topSubRaddit2">
+      <img src="../assets/images/100pxBurriedRaddit.png" />Apéro a 17h30 chez Arthur 🍾
+    </div>
+    <div class="ctnr_field topSubRaddit3">
+      <img src="../assets/images/230pxBurriedRaddit.png" />1001 recettes autour du Radis
+    </div>
+    <div class="ctnr_field topSubRaddit4">
+      <img src="../assets/images/150pxBurriedRaddit.png" />Radineurs
+    </div>
+  </div>
+  <body>
+    <LeftPanelComponent class="hidden" />
+    <section class="page">
       <!-- *** POSTS *** -->
-        <div class="flex flex-col  w-[78%] m-auto mb-5" v-if="subradditStore.posts != undefined">          
-          <div v-for="posty in subradditStore.posts">
+      <div class="flex flex-col w-[78%] m-auto mb-5" v-if="subradditStore.posts != undefined">
+        <div v-for="posty in subradditStore.posts">
           <div v-for="post in posty" class="pb-10">
-            <Post
-            :post=post
-            />
+            <Post :post="post" />
           </div>
-          </div>
-          
+        </div>
       </div>
-    
     </section>
-</body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  </body>
 
   <!-- {{ subradditStore.subradInfo.id }}
   <br />
