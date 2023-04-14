9c5c1f25e41409bac1b5fa2f7be1fc6c7db049c0
jan <jan.mondelaers@epitech.eu>
2023-04-14 14:04:24 +0200

    fiex

--- front-raddit/src/components/PostComponent.vue
+++ front-raddit/src/components/PostComponent.vue
@@ -10,10 +10,14 @@ function genUrl(id)
 {
  return "/p/" + id;
 }
+function genUrls(id)
+{
+ return "/r/" + id;
+}
+
 </script>
 
 <template>
-
     <div class="flex flex-row bg-green-100 w-[100%] text-sm">
         <div class="w-16 bg-green-100 w-[7%] m-2" >
             <vote
@@ -26,7 +30,7 @@ function genUrl(id)
                 <div class="w-[5.5%] rounded-xl bg-green-100">
                     <img src="../assets/images/wholeRaddit.png" />
                 </div>
-                <div class="font-bold">{{ post.sname }}</div> <span class="ml-2 mr-2"> posted by </span> {{ post.uname }}  
+                <RouterLink :to="genUrls(post.sname)" ><div class="font-bold">{{ post.sname }}   </div>     </RouterLink><span class="ml-2 mr-2"> posted by </span> {{ post.uname }}  
             </div>
 
             <RouterLink :to="genUrl(post.id)" >
